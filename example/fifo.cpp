327d366de6485c788091f75eb03f437ea2339e31
Yukariko <rkdals7979@naver.com>
2017-03-08 21:00:43 +0900

    fix fifo consider interrupt

--- example/fifo.cpp
+++ example/fifo.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 {
     int ncpu = 1;
     int nproc = 10;
-    int timeSlice = 3000;
+    int timeSlice = 30;
     int runtime = 0;
 
     SchedTest st(ncpu);
@@ -22,14 +22,29 @@ int main(int argc, char **argv)
 
     while(!procs.empty())
     {
+        int select = -1;
         for(auto it = procs.begin(); it != procs.end(); it++)
         {
+            if (st.getState(*it) == INTERRUPT)
+                continue;
+            select = *it;
             st.setFirst(*it, timeSlice);
             auto ret = st.commit();
 
             if(ret.second == EXIT)
             {
                 it = procs.erase(it);
+            }
+        }
+
+        if (select == -1)
+        {
+            st.setFirst(*procs.begin(), timeSlice);
+            auto ret = st.commit();
+
+            if(ret.second == EXIT)
+            {
+                procs.erase(procs.begin());
             }
         }
         printf("fairness : %.2f%%\n", st.getFairness() * 100);
